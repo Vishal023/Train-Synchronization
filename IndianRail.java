bb537ebcad825ab0b3c44ee623e8f8e3eff68510
Vishal Singh <Vishal023@users.noreply.github.com>
2020-03-28 16:23:27 +0530

    Initial Commit

---
+++ IndianRail.java
@@ -0,0 +1,40 @@
+package OsProject;
+
+/**
+ * IndianRail has decided to improve its efficiency by automating not just its trains but also its
+ * passengers. Each passenger and each train is controlled by a thread. You have been hired to write
+ * synchronization functions that will guarantee orderly loading of trains. You must define a
+ * structure struct station, plus several functions described below.
+ *
+ * @author Vishal Singh
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class IndianRail{
+
+    static int totalNumberOfPassenger;
+    static int noOfFreeSeatsInTheTrain;
+
+    static class Station {
+
+        int passengersAtTheStation;
+        int passengersInTrain;
+        int totalBoarderPassengers;
+        final Lock lock;
+        final Condition passengerSeatedCondition;
+
+        Station(Lock lock,Condition passengerSeatedCondition) {
+            passengersAtTheStation = 0;
+            totalBoarderPassengers = 0;
+            passengersInTrain = 0;
+            this.lock = lock;
+            this.passengerSeatedCondition = passengerSeatedCondition;
+        }
+    }
+
+}
